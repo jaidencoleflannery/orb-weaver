5e5d12a405f9ab2eb3e5f2861a928e2c2eb19433
jaidencoleflannery <jaidencoleflannery@gmail.com>
2026-06-13 23:50:24 -0500

    http | parsing for type and headers.

--- src/http-server/services/http-handler/http_handler.c
+++ src/http-server/services/http-handler/http_handler.c
@@ -1,39 +1,78 @@
 #include <sys/socket.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "services/logging/logging.h"
 
 #include "./http_handler.h"
 
-// break request into string per line array.
-static bool get_metadata(char *message, size_t message_size, http_request *result) {
-    if(message == NULL || result == NULL) {
-        ERROR_LOG("process_http_request: Parameter provided was null.");
+static bool validate_http_type(char *type, size_t type_size) {
+    if(type == NULL) {
+        ERROR_LOG("validate_http_type: Provided type parameter was invalid.");
         return false;
     }
 
-    char current_line[MAX_HTTP_HEADER_SIZE];
+    return true;
+}
+
+static bool validate_http_headers(char *type, size_t type_size) {
+    if(type == NULL) {
+        ERROR_LOG("validate_http_type: Provided type parameter was invalid.");
+        return false;
+    }
+
+    return true;
+}
+
+// request metadata (line 0) is considered a header here.
+static bool get_http_metadata(char *message, size_t message_size, http_request *result) {
+    if(message == NULL || result == NULL || message_size == 0) {
+        ERROR_LOG("get_http_metadata: Parameter provided was invalid.");
+        return false;
+    }
+
+    char header_line[MAX_HTTP_HEADER_SIZE];
+    size_t line_increment = 0; // to validate entry in type header line.
     char *message_cursor = message;
     size_t num_parsed = 0;
     bool is_malformed = true;
 
     // get first line (type metadata).
-    while(message_cursor != NULL 
-        && *message_cursor != '\n' // end of line, conditional validation within block is reliant on this.
-        && num_parsed < MAX_HTTP_HEADER_SIZE 
-    ) {
-        if(*message_cursor == '\r' && is_malformed == true) {
-            is_malformed = false;
-            continue;
+    // '\n' denotes end of line, conditional validation within block is reliant on this.
+    while(message_cursor != NULL && *message_cursor != '\n') {
+        if(num_parsed == MAX_HTTP_HEADER_SIZE ) {
+            ERROR_LOG("get_http_metadata: Provided HTTP request's header value exceeded the maximum length of %d.", MAX_HTTP_HEADER_SIZE);
+            return false;
         } else if(is_malformed == false) {
-            // if statement catches for newline, '\r' is invalid in http otherwise.
             // TODO: setup a response service for bad requests.
             ERROR_LOG("process_http_request: Provided HTTP request line was malformed.");
             return false;
+        } else if(*message_cursor == '\r') {
+            // '\r' marks the new carriage and is only valid for the end of a line (ie header value).
+            // '\r' must be followed by '\n'.
+            is_malformed = false;
+            continue;
         }
 
-        current_line[num_parsed] = *message_cursor;
+        if(*message_cursor == ' ') {
+            bool validation_result = false;
+            // if first line, get type.
+            if(line_increment < 1)
+                validation_result = validate_http_type(header_line, num_parsed);
+            else
+                validation_result = validate_http_headers(header_line, num_parsed);
+
+            if(!validation_result) {
+                ERROR_LOG("get_http_metadata: Provided HTTP request's headers vere invalid.");
+                return false;
+            }
+
+            // clear values so next iteration can grab headers.
+            memset(header_line, 0, sizeof(header_line));
+        }
+
+        header_line[num_parsed] = *message_cursor;
         ++num_parsed;
         ++message_cursor;
     }
